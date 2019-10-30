c3e689170c4c4829f59d98caa34d57489ad537a9
emil <eostlin@kth.se>
2019-10-30 11:59:21 +0200

    Added readme and a comment for the route

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # dbtest
+
+## Overview
+Wrote this code within a few hours, it has tests written for it and passes all the tests on Deambroker's side :)
+
+## Stack
+
+* Web API built using Express JS
+* Functional Testing using Jest 
+* API testing using Supertest 
+
--- index.js
+++ index.js
@@ -9,7 +9,11 @@ var server = require('http').createServer(app)
 var port = process.env.PORT || 3001
 
 
+/**
+ * Route for Analyze, will take a piece of text from a Post and return a Json with information about the text.
+ */
 app.post('/analyze', (req,res) => {
+
     if(req.body.text === undefined) {
         res.status(400)
         res.send("Error no input found")
